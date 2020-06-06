70ce840a3e72537c492a54fca66a814fb3a5ff9e
Boris Nandi <borisnandi@Boriss-MacBook-Pro.local>
2020-06-06 17:40:29 +1000

    Updates

---
+++ precache-manifest.22162b880928f76750b252c8f6c68186.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "685135ad2fef3f0c1301d6d369c35e97",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6007605792fd40cd0791",
+    "url": "/monsters-rolodex/static/css/main.7f1e34af.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6007605792fd40cd0791",
+    "url": "/monsters-rolodex/static/js/main.898f91e0.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
